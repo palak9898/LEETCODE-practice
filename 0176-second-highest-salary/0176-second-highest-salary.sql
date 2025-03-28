3a1018c9b45b09567d0de92ca0e386f2fa2cec98
palak9898 <160301117+palak9898@users.noreply.github.com>
2025-03-28 10:50:48 +0530

    Time: 284 ms (92.09%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from (
-    select salary, dense_rank() over (order by salary desc) as rak from Employee 
-) ranked where rak =2;
+SELECT 
+(select distinct (salary ) 
+     FROM Employee 
+     ORDER BY salary DESC LIMIT 1 OFFSET 1 ) as SecondHighestSalary;
